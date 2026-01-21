6ec07e805fe1649fb5f7e56c6bf1c65ce5c4af6d
Tijmen Toxopeus <tijmen.toxo@gmail.com>
2026-01-21 22:02:51 +0100

    added enums

---
+++ training_app/domain/enums.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class Sport(str, Enum):
+    RUN = "run"
+
+
+class WorkoutType(str, Enum):
+    EASY = "easy"
+    LONG = "long"
+    TEMPO = "tempo"
+    INTERVALS = "intervals"
+    RECOVERY = "recovery"
+    REST = "rest"
